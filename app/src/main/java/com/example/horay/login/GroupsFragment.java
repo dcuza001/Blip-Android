70c0a3644f1a737bcd4d371e8fafffe267e3eabb
dcuza001 <dcuza001@ucr.edu>
2016-05-06 16:20:57 -0700

    Drawer thing
    drawer

---
+++ app/src/main/java/com/example/horay/login/GroupsFragment.java
@@ -0,0 +1,22 @@
+package com.example.horay.login;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Daniel on 5/3/2016.
+ */
+public class GroupsFragment extends Fragment{
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.groups_layout, container, false);
+        return myView;
+    }
+}
