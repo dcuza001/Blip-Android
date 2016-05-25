08d2294c92aaeb0467fbeb173770ccbccf3d590b
Raymond Ho <rho002@ucr.edu>
2016-05-25 16:04:54 -0700

    More stuff

--- app/src/main/java/com/example/horay/login/MenuActivity.java
+++ app/src/main/java/com/example/horay/login/MenuActivity.java
@@ -5,30 +5,84 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 public class MenuActivity extends AppCompatActivity {
 
     MenuActivityRecyclerAdapter adapter;
     RecyclerView recyclerView;
+    DatabaseReference ref = FirebaseDatabase.getInstance()
+            .getReferenceFromUrl("https://blipster.firebaseio.com/");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        ArrayList<MenuActivityRecyclerInfo> data = new ArrayList<>();
-        data.add(new MenuActivityRecyclerInfo("BOB"));
-        data.add(new MenuActivityRecyclerInfo("Someone"));
-        data.add(new MenuActivityRecyclerInfo("dsfd"));
-        data.add(new MenuActivityRecyclerInfo("Someone else"));
-        data.add(new MenuActivityRecyclerInfo("BOB"));
-        data.add(new MenuActivityRecyclerInfo("Someone"));
-        data.add(new MenuActivityRecyclerInfo("dsfd"));
-        data.add(new MenuActivityRecyclerInfo("Someone else"));
-        data.add(new MenuActivityRecyclerInfo("BOB"));
-        data.add(new MenuActivityRecyclerInfo("Someone"));
-        data.add(new MenuActivityRecyclerInfo("dsfd"));
-        data.add(new MenuActivityRecyclerInfo("Someone else"));
+        final ArrayList<MenuActivityRecyclerInfo> data = new ArrayList<>();
+
+        //final Semaphore semaphore = new Semaphore(0);
+        final String username = FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
+        //TODO:
+        if(username != null) {
+            ref.child("clients").child(username).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot usersSnapshot) {
+
+                    if (usersSnapshot.child("following").exists()) {
+
+                        ref.child("clients").child(username).child("following").addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot usersSnapshot) {
+
+                                for (DataSnapshot userSnapshot : usersSnapshot.getChildren()) {
+                                    String friend = userSnapshot.getKey().toString();
+
+                                    data.add(new MenuActivityRecyclerInfo(friend));
+                                }
+
+                                //Inflate Recycler View Object
+                                recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+
+                                //Create Adapter
+                                adapter = new MenuActivityRecyclerAdapter(MenuActivity.this, data);
+
+                                //Set Layout Manager
+                                recyclerView.setLayoutManager(new LinearLayoutManager(MenuActivity.this));
+
+                                //Set Adapter
+                                recyclerView.setAdapter(adapter);
+
+
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+
+
+                        });
+
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+
+
+            });
+        }
 
         //Inflate Recycler View Object
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
@@ -41,6 +95,6 @@ public class MenuActivity extends AppCompatActivity {
 
         //Set Adapter
         recyclerView.setAdapter(adapter);
-
     }
+
 }
