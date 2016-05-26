2ba52c6db54cfa9862088801729069316c17ddf0
ryosaito999 <ryocsaito@gmail.com>
2016-05-26 03:14:29 -0700

    Revert "slightly redesigned view blip"
    This reverts commit f2997faf9ea7b675e479d0dd2d2ac2238d742440.

--- app/src/main/java/com/example/horay/login/ViewBlipDialog.java
+++ app/src/main/java/com/example/horay/login/ViewBlipDialog.java
@@ -172,7 +172,7 @@ public class ViewBlipDialog extends DialogFragment {
         tagView = (TextView)view.findViewById(R.id.TagView);
 
         followButton = (Button) view.findViewById(R.id.buttonFollow);
-        replyButton = (Button) view.findViewById(R.id.post_reply);
+        replyButton = (Button) view.findViewById(R.id.replyButton);
 
         if(blip.replies != null)
 
